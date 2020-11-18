d388538e7c9b4680847e39b592831d4c2fa3f5b7
Adrian1118 <adrian.vk@hotmail.fr>
2020-11-18 17:59:11 +0100

    Made modifications to the EE data gathering script

--- group31project/scripts/ee_api_lookup.R
+++ group31project/scripts/ee_api_lookup.R
@@ -55,52 +55,89 @@ build_full_df_by_API <- function(method,
                    "format" = format,
                    "parameters" = parameters) # Additional method-specific parameters, see documentation.
   
-  
   # Pre-assign the output to the first query (100 observations)
-  
-  api_list$parameters <- "&offset=0"
-  df <- read.csv(paste(api_list, collapse=""))
+  url <- paste(api_list, collapse="")
+  df <- read.csv(url)
   
   # Iterate queries until the maxoffset set by the function call (default 100)
   
   for (offset in seq(from = 100, to = maxoffset, by = 100)) {
-    api_list$parameters <-
-      paste(c("&offset=", as.character(offset)), collapse = "")
-    df.next <- read.csv(paste(api_list, collapse = ""))
+    url <- paste(c(url,
+                   "&offset=", 
+                   as.character(offset)), 
+                 collapse = "")
+    df.next <- try(read.csv(url),
+                   silent=TRUE)
+    if (inherits(df.next, "try-error")) break
+    
     df <- rbind(df, df.next)
-    Sys.sleep(1) # We can only request an API once a second (due website's limitations)
+    Sys.sleep(1.5) # We can only request an API once a second (due website's limitations)
   }
   
   return(df)
 }
   
 
-
-### Teams earnings overall
-
-# CAUTION: will take about 12 seconds to compute
-
-df.team_earnings <- build_full_df_by_API(method = methods_list$topteamsearning,
-                                         maxoffset = 1200)
-
-write.csv(df.team_earnings, "data/EE_teams_earnings.csv")
-
-
-### Tournaments prize cash overall
-
-# CAUTION: will take about 412 seconds to compute
-
-df.tournaments <- build_full_df_by_API(method = methods_list$recenttournaments,
-                                       maxoffset = 41200) 
-
-write.csv(df.tournaments, "data/EE_tournaments.csv")
-
 # 
+# ### Teams earnings overall
+# 
+# # CAUTION: will take more than 12 seconds to compute
+# 
+# # df.team_earnings <- build_full_df_by_API(method = methods_list$topteamsearning,
+#                                          maxoffset = 1200)
+# 
+# # write.csv(df.team_earnings, "data/EE_teams_earnings.csv")
+# 
+# 
+# ### Tournaments prize cash overall
+# 
+# # CAUTION: will take more than 412 seconds to compute
+# 
+# # df.tournaments <- build_full_df_by_API(method = methods_list$recenttournaments,
+#                                        maxoffset = 41200) 
+# 
+# # write.csv(df.tournaments, "data/EE_tournaments.csv")
+# 
+# 
+# 
+# 
+# ### Team earnings for League of Legends
+# 
+# # CAUTION: will take more than 3 seconds to compute
+# 
+# df.lol_team_earnings <- build_full_df_by_API(method = methods_list$topteamsearn_bygame,
+#                                          maxoffset = 300,
+#                                          parameters = "&gameid=164")
+# 
+# write.csv(df.lol_team_earnings, "data/EE_lol_teams_earnings.csv")
+# 
+# 
+# ### Team earnings for Dota 2
+# 
+# # CAUTION: will take more than 3 seconds to compute
+# 
+# df.dota2_team_earnings <- build_full_df_by_API(method = methods_list$topteamsearn_bygame,
+#                                              maxoffset = 100,
+#                                              parameters = "&gameid=231")
+# 
+# write.csv(df.dota2_team_earnings, "data/EE_dota2_teams_earnings.csv")
+# 
+#
+# Find all charts about team earnings by game
+#
 
 
-# Trying team earnings for League of Legends
 
+#############################################
+########### TEST AREA #######################
+#############################################
+# Trying team earnings for League of Legends#
+
+
+versionnumber <- "0"
 method <- methods_list$topteamsearn_bygame
+apikey <- "377aeceec830e58cdce6d6f09a365a6c2e438a01160f1ac4a85b1272106dcea7"
+format <- "csv"
 
 api_list_lol <- list("domain" = "http://api.esportsearnings.com/v",
                  "versionnumber" = versionnumber,
@@ -115,5 +152,15 @@ api_list_lol <- list("domain" = "http://api.esportsearnings.com/v",
 
 # Pre-assign the output to the first query (100 observations)
 
-api_list_lol$parameters <- "&gameID=164&offset=0"
-df1 <- read.csv(paste(api_list_lol, collapse=""))
+games <- read.csv("data/EE_tournaments.csv") %>%
+  distinct(GameId)
+
+for(g in games$GameId[1:10]){
+  api_list_lol$parameters <- paste(c("&gameid=", g), collapse="")
+  df.temp <- build_full_df_by_API(method = api_list_lol$methodname, maxoffset=1000, parameters = api_list_lol$parameters)
+  write.csv(df.temp, paste(c("data/EE_", g, "_teams_earnings.csv"), collapse=""))
+  Sys.sleep(1.5)
+}
+
+  df6 <- build_full_df_by_API(method = api_list_lol$methodname, maxoffset=1000, parameters = api_list_lol$parameters)
+
