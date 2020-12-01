a0e802a9f947858637ea3a6234b7a61d5db33f50
Adrian1118 <adrian.vk@hotmail.fr>
2020-12-01 13:31:48 +0100

    Removed static paths

--- group31project/scripts/EE_EDA-01.R
+++ group31project/scripts/EE_EDA-01.R
@@ -7,18 +7,20 @@
 library(tidyverse)
 library(scales)
 
-setwd("C:/Users/adria/Desktop/projects/dfsba_group31_project/group31project")
-
 # General Earnings in Esports
 
 earnings_pergame <- read.csv("data/EsportsEarnings.com/EE_games_earnings.csv")
 
 # ------------------------------------------------------------------------------
+# MOST PRIZE MONEY WON
+# ------------------------------------------------------------------------------
 
 o_prizes <- earnings_pergame %>%
-  select(GameName, TotalUSDPrize) %>%
-  arrange(TotalUSDPrize, desc=TRUE) %>%
-  filter(TotalUSDPrize > quantile(TotalUSDPrize, p=0.95, na.rm=TRUE))
+  select(id, GameName, TotalUSDPrize) %>%
+  filter(TotalUSDPrize > quantile(TotalUSDPrize, p=0.90, na.rm=TRUE)) %>%
+  arrange(desc(TotalUSDPrize))
+
+write.csv(o_prizes, "data/top_prizes.csv")
 
 o_prizes %>%
   ggplot(aes(x=reorder(GameName, TotalUSDPrize), y=TotalUSDPrize/1000000)) +
@@ -28,11 +30,15 @@ o_prizes %>%
   coord_flip()
 
 # ------------------------------------------------------------------------------
+# MOST TOURNAMENTS ORGANIZED
+# ------------------------------------------------------------------------------
 
 o_tourn <- earnings_pergame %>%
-  select(GameName, TotalTournaments) %>%
-  arrange(TotalTournaments, desc=TRUE) %>%
-  filter(TotalTournaments > quantile(TotalTournaments, p=0.95, na.rm=TRUE))
+  select(id, GameName, TotalTournaments) %>%
+  arrange(desc(TotalTournaments)) %>%
+  filter(TotalTournaments > quantile(TotalTournaments, p=0.90, na.rm=TRUE))
+
+write.csv(o_tourn, "data/top_nb_tourn.csv")
 
 o_tourn %>%
   ggplot(aes(x=reorder(GameName, TotalTournaments), y=TotalTournaments)) +
@@ -40,11 +46,15 @@ o_tourn %>%
   coord_flip()
 
 # ------------------------------------------------------------------------------
+# MOST PROFESSIONAL PLAYERS
+# ------------------------------------------------------------------------------
 
 o_players <- earnings_pergame %>%
-  select(GameName, TotalPlayers) %>%
-  arrange(TotalPlayers, desc=TRUE) %>%
-  filter(TotalPlayers > quantile(TotalPlayers, p=0.95, na.rm=TRUE))
+  select(id, GameName, TotalPlayers) %>%
+  arrange(desc(TotalPlayers)) %>%
+  filter(TotalPlayers > quantile(TotalPlayers, p=0.90, na.rm=TRUE))
+
+write.csv(o_players, "data/top_nb_players.csv")
 
 o_players %>%
   ggplot(aes(x=reorder(GameName, TotalPlayers), y=TotalPlayers)) +
